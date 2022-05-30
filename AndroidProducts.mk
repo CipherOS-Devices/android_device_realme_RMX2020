d18211f8c780df83a7e7ee11a91003b652838dba
Sarthak Roy <sarthakroy2002@gmail.com>
2022-05-30 08:46:17 +0530

    RMX2020: CipherOS Bringup
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I9c8168d673290bb7fb25a5a8f7362433f21a9a32

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX2020.mk
+    $(LOCAL_DIR)/cipher_RMX2020.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX2020-user \
-    arrow_RMX2020-userdebug \
-    arrow_RMX2020-eng
+    cipher_RMX2020-user \
+    cipher_RMX2020-userdebug \
+    cipher_RMX2020-eng
--- cipher_RMX2020.mk
+++ cipher_RMX2020.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common CipherOS stuff.
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_RMX2020
+PRODUCT_NAME := cipher_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme Monet
@@ -23,8 +23,9 @@ PRODUCT_MANUFACTURER := realme
 
 TARGET_BOOT_ANIMATION_RES := 720
 
-# ArrowOS additions
-DEVICE_MAINTAINER := sarthakroy2002
+# CipherOS additions
+CIPHER_OFFICIAL := true
+CIPHER_MAINTAINER := sarthakroy2002
 
 # Build info
 BUILD_FINGERPRINT := "google/coral/coral:12/SP2A.220505.002/8353555:user/release-keys"
